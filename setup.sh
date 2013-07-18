5d20cf7e4cfb10f892966737230f8d597b9929fa
mgp <mgp-code@linuxupc.upc.es>
2013-07-19 01:25:26 +0200

    Setup.sh: Added apt-get update and install curl

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
+sudo apt-get update
+sudo apt-get install -y curl
 sudo apt-get install -y git-core
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
